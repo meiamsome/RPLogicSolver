41a808b0bc6046e8c5c55a45e3aa59c2868c4fed
Oliver Wright <meiamsome@hotmail.com>
2014-01-11 22:07:56 +0000

    MrExcel Fixed everything and I am commiting under my name <3

--- RPiLogicApi.py
+++ RPiLogicApi.py
@@ -5,24 +5,27 @@ import RPi.GPIO as GPIO
 import itertools
 import time
 
-GPIO.setmode(GPIO.BCM)
 
 def getTruthtable(outputPins, inputPin):
+	GPIO.setmode(GPIO.BCM)
 	truthtable = dict()
 
 	for pin in outputPins:
 		GPIO.setup(pin, GPIO.OUT)
 
-	GPIO.setup(input_pin, GPIO.IN)
+	GPIO.setup(inputPin, GPIO.IN)
 
 	vals = [True, False]
-	carthProd = list(itertools.product([vals for i in range(outputPins)]))
+	carthProd = list(itertools.product(*[vals for i in range(len(outputPins))]))
 
 	for oPinVals in carthProd:
 		for i, val in enumerate(oPinVals):
 			GPIO.output(outputPins[i], val)
 
-		time.sleep(0.001)
+		time.sleep(1)
 		truthtable[oPinVals] = GPIO.input(inputPin)
 
 	return truthtable
+	GPIO.cleanup()
+
+while(1): print (getTruthtable([9, 11], 10))
